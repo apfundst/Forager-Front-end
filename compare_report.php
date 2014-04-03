edf32cb1b010838344ceea5225e8468213505c46
firefox0102 <firefox0102@gmail.com>
2014-04-03 12:14:49 -0400

    Compare Report Page

---
+++ compare_report.php
@@ -0,0 +1,112 @@
+<html>
+<head>
+<title>Forager: Compare Report</title>
+<link rel="stylesheet" type="text/css" href="css/dashboard.css">
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.0.js"></script>
+<script type="text/javascript" src="js/session.js"></script>
+
+
+</head>
+<body>
+
+<div class="container_fluid">
+<nav class="navbar navbar-default" role="navigation">
+<a href="dashboard.html"><img src="img/logo.png" width="400" style="margin-top:8px; margin-bottom:8px;"/></a>
+<div style="color:white; float:right;">
+     <a href="dashboard.html"><button  class="menu" >Home</button></a>
+      <button id="log_out" class="menu">Logout</button><br>
+      <span id="user_logged_in"></span>
+</div>
+</nav>
+
+<div class="row">
+<div class="col-lg-12">
+<div class="panel">
+  <div class="panel_body">
+  </div>
+  <div class="panel_body" id="report_info">
+  </div>  
+  <div class="panel_body">
+  <div class="panel_heading"> Compare Reports </div>
+<script type="text/javascript">
+</script>
+<br>
+<center><p>
+  <a class="tab" href="#item1">Tree Report</a>
+  <a class="tab" href="#item2">Graph Report</a>
+  </p>
+<hr></center> 
+
+<div class="items">
+  <div id="item1">
+    <table id="goups_background">
+    <ul class="tree" style="float:left;  border-right:2px solid black;">
+    	Report 1 Title
+      <li>
+          <a>domain</a>
+          <ul>
+              <li><a>pages</a>
+          <ul>
+              <li>errors</li>  
+              <li>errors</li>   
+              <li>error</li>            
+          </ul>   </li>  
+              <li>errors</li>  
+              <li>errors</li>  
+              <li>errors</li>            
+          </ul>                
+      </li>
+      
+      <li>
+          <a>domain</a>
+          <ul>
+              <li>page</li>  
+              <li>page</li>      
+          </ul>                
+      </li>
+  </ul>
+  <ul class="tree" style="float:right; border-left:2px solid black;">
+  	   Report 2 Title
+      <li>
+          <a>domain</a>
+          <ul>
+              <li><a>pages</a>
+          <ul>
+              <li>errors</li>  
+              <li>errors</li>  
+              <li>error</li>            
+          </ul>   </li>  
+              <li>errors</li>  
+              <li>errors</li>  
+              <li>errors</li>            
+          </ul>                
+      </li>
+      
+      <li>
+          <a>domain</a>
+          <ul>
+              <li>page</li>  
+              <li>page</li>      
+          </ul>                
+      </li>
+  </ul>
+  </table>
+  </div>
+
+  <div id="item2">
+  <table id="goups_background">
+	graph stuff goes here
+  </table>
+    </div>
+  
+  <div id="default" > <h1>Select a report type above</h1></div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+</body>
+</html>
