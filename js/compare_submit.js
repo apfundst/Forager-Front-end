f07b5e848cfb5a5113e51eed9d234447173388d6
Andrew Pfundstein <drew@myskiprofile.com>
2014-04-09 20:44:08 -0400

    bug fix

--- js/compare_submit.js
+++ js/compare_submit.js
@@ -1,6 +1,6 @@
 //post to a php page
 var Compare = {
-	Submit: function(rp1, rp2)
+	submit: function(rp1, rp2)
 	{
 	    	$.post("includes/get_compare_reports.php",{cpreport1: rp1, cpreport2: rp2});
 	    	window.location = "compare_report.html";
