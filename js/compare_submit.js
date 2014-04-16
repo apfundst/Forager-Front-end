0c14cd653da84ef95e8cde2ddc53bccf21f8779e
Andrew Pfundstein <drew@myskiprofile.com>
2014-04-16 19:08:02 -0400

    PEETA

--- js/compare_submit.js
+++ js/compare_submit.js
@@ -1,6 +1,6 @@
 //post to a php page
 var Compare = {
 	Submit: function(rp1, rp2){
-    	window.location = "compare_report.html?id1=" + rp1 + "&id2=" +rp2;
+    	window.location.href = "compare_report.html?id1=" + rp1 + "&id2=" +rp2;
     }
 }
