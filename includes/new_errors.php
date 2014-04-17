086c192f2c34ed48b4712e2cd7837cfdac96cff5
jcathcart1 <jcathcart1@gmail.com>
2014-04-16 20:53:30 -0400

    You can get with this....or you can get with that
    Only returns URL's from scan_id1 that have errors not in scan_id2

--- includes/new_errors.php
+++ includes/new_errors.php
@@ -1,27 +1,23 @@
 <?php
 	$scan_id1 = $_POST["rp1"];
 	$scan_id2 = $_POST["rp2"];
-
-	session_start();
 	$con = mysqli_connect("localhost","root","forageme","forager");
 	if (mysqli_connect_errno()){
 	  echo "Failed to connect to server" . mysqli_connect_error();
 	  exit;
 	}
 
-	//query database for ALL scan data
-	$scan_id1 = mysqli_real_escape_string($con,$scan_id1);
-	$scan_id2 = mysqli_real_escape_string($con,$scan_id2);
-
 	$table1 = "url" . $scan_id1;
 	$table2 = "url" . $scan_id2;
 
 	$sql = "
-	
-	NEED SQL FOR THIS LOLOLOLOLOOLOLOLOLOLOOLOLOLOLOLOLOLOLOLOOL
-
+		SELECT  '$table1'.url, '$table1'.domain, '$table1'.status_code_type
+		FROM `'$table1'`
+		JOIN `'$table2'`
+		ON   '$table1'.url = '$table2'.url AND '$table1'.state = 1 AND '$table2'.state = 0; 
 	";
 		
+	$arr = array();
 	$result = mysqli_query($con,$sql);
 	$row = mysqli_fetch_array($result, MYSQLI_BOTH);
 		
@@ -31,10 +27,10 @@
 		{
 			$arr[] = $temp;
 		}
-		echo json_encode((array)$arr)
+		echo json_encode((array)$arr);
 	}
 	else
 	{
-		
+		echo "NO DIFFERENCES";
 	}	
 ?>
