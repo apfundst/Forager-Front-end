444984ba79f277ef9e134d8f975b221fb5878d46
jcathcart1 <jcathcart1@gmail.com>
2014-04-18 00:27:06 -0400

    I want you to hit me in the face as hard as you can
    can

--- includes/get_report.php
+++ includes/get_report.php
@@ -1,20 +1,18 @@
 <?php
 $scan_id = $_POST('id');
+$func_id = $_POST('funcId');
 
-$con = mysqli_connect("localhost","root","forageme","forager");
+$con = mysqli_connect("localhost","root","forageme","db_forager");
 if (mysqli_connect_errno()){
   echo "Failed to connect to server.....can you be cool just once! Just once be cool!: " . mysqli_connect_error();
   exit;
 }
 
-//query database for scan data
-function get_report_data($scan_id)
-{
-	$table = "url" . $post_id;
-
-
+// query database for scan data
+// get_report_data
+if( $func_id == 1){
 	$sql = "
-		SELECT start_time, stop_time, pages_scanned, number_errors
+		SELECT scan_name, start_time, stop_time, pages_scanned, number_errors
 		FROM `scan`
 		WHERE scan_id = '$scan_id';
 	";
@@ -22,11 +20,11 @@ function get_report_data($scan_id)
 	$result = mysqli_query($con,$sql);
 	$row = mysqli_fetch_array($result, MYSQLI_BOTH);
 	
+	$scan_name = $row["scan_name"];
 	$start_time = $row["start_time"];
 	$stop_time =  $row["stop_time"];
 	$pages_scanned = $row["pages_scanned"];
 	$number_errors = $row["number_errors"];
-
 
 	// Calculate Elapsed Time
 	$elapsed_time = date_diff($stop_time, $start_time);
@@ -34,51 +32,27 @@ function get_report_data($scan_id)
 	//Calculate AVG errors per page
 	$avg_errors_per_page = $number_errors/$pages_scanned;
 	//Calculate Percent Pages with Errors
-	$pages_with_errors = $pages_scanned/$number_errors)
-
-	if(!is_null($row))
-	{
-		while($temp = mysqli_fetch_assoc($result))
-		{
-			$arr[] = $temp;
-		}
-		echo json_encode((array)$arr)
-	}
-	else
-	{
-		echo "400";
-	}
+	//$pages_with_errors = $pages_scanned/$number_errors);
+	
+	$arr = array($scan_name, $start_time,$stop_time, $elapsed_time, $pages_scanned, $number_errors, $avg_errors_per_page);	
+	echo json_encode((array)$arr);	
 }
-
-//graph data
-//number of total errors
-//scan time
-//total links scanned
-function get_graph_data()
-{
-	$report_id = mysqli_real_escape_string($con,$report_id);
-
+// query database for other data
+// get_url_data
+// url, domain, status_code, status_code_message,
+if( $func_id == 2 ){
+	$table = "url" . $scan_id;
 	$sql = "
-	SELECT number_errors, pages_scanned, start_time, stop_time
-	FROM `url`
-	WHERE '$report_id' = scan_id
+		SELECT url, domain, status_code, status_code_type
+		FROM `'$table'`
 	";
 	
 	$result = mysqli_query($con,$sql);
-	$row = mysqli_fetch_array($result, MYSQLI_BOTH);
-	
-	if(!is_null($row))
-	{
-		while($temp = mysqli_fetch_assoc($result))
-		{
-			$arr[] = $temp;
-		}
-		echo json_encode((array)$arr)
+	$arr = array();
+	while( $row = mysqli_fetch_array($result, MYSQLI_BOTH)  ){
+		$arr[] = $row;
 	}
-	else
-	{
-		echo "400";
-	}
+	echo json_encode((array)$arr);
 }
-
+ 
 ?>
