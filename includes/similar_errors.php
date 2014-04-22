164e80d65416ae5166c3baa6f784ff8b409b559e
jcathcart1 <jcathcart1@gmail.com>
2014-04-21 21:06:53 -0400

    So say we all.... SO SAY WE ALL!!!!!

--- includes/similar_errors.php
+++ includes/similar_errors.php
@@ -1,6 +1,6 @@
 <?php
-	$scan_id1 = $_POST["rp1"];
-	$scan_id2 = $_POST["rp2"];
+	$scan_id1 = $_POST["rep1"];
+	$scan_id2 = $_POST["rep2"];
 
 	session_start();
 	$con = mysqli_connect("localhost","root","forageme","forager");
@@ -9,34 +9,29 @@
 	  exit;
 	}
 
-	//query database for ALL scan data
-	$scan_id1 = mysqli_real_escape_string($con,$scan_id1);
-	$scan_id2 = mysqli_real_escape_string($con,$scan_id2);
-
 	$table1 = "url" . $scan_id1;
 	$table2 = "url" . $scan_id2;
 
 	$sql = "
 		SELECT *
-		FROM `'$table1'`
-		INNER JOIN `'$table2'`
-		ON '$table1'.url = '$table2'.url
-		GROUP BY '$table1'.domain ORDER BY '$table1'.url DESC;
+		FROM `$table1`
+		INNER JOIN `$table2`
+		ON $table1.url = $table2.url
+		GROUP BY $table1.domain ORDER BY $table1.url DESC;
 	";
 		
-	$result = mysqli_query($con,$sql);
-	$row = mysqli_fetch_array($result, MYSQLI_BOTH);
-		
+	$arr = array();
+	$result = mysqli_query($con,$sql);	
 	if(!is_null($row))
 	{
 		while($temp = mysqli_fetch_assoc($result))
 		{
 			$arr[] = $temp;
 		}
-		echo json_encode((array)$arr)
+		echo json_encode((array)$arr);
 	}
 	else
 	{
-		
+		echo "NO DIFFERENCES";
 	}	
 ?>
--- includes/stored_procedures.sql
+++ includes/stored_procedures.sql
@@ -3,47 +3,76 @@ CREATE PROCEDURE url_check(IN url_table varchar(1000), IN link_rel_table varchar
 						   IN this_source varchar(1000), IN this_link varchar(1000), 
 						   IN this_url varchar(1000), IN this_domain varchar(1000),
 						   IN this_dest_id int(11), INOUT inserted_url_id int(11) )						    
-    BEGIN
-        DECLARE checked_url_id int;
-        SELECT url_id INTO checked_url_id
-        FROM url_table 
-        WHERE source = this_source AND link = this_link;
-        IF checked_url_id IS NOT NULL THEN
-        	INSERT INTO link_rel_table(`url_id`,`dest_id`)
-        	VALUES(checked_url_id , this_dest_id );
+		BEGIN
+		SET @url_table = url_table;
+		SET @link_rel_table = link_rel_table;
+		SET @this_source = this_source;
+		SET @this_link = this_link;
+		SET @this_url = this_url; 
+		SET @this_url = this_domain;
+		SET @this_dest_id = this_dest_id;
+		SET @inserted_url_id = inserted_url_id;
+		SET @temp_id = NULL;
+
+    	SET @checked_url_id = CONCAT('SELECT url_id INTO @temp_id FROM `?` WHERE source = ? AND link = ?');
+		PREPARE stmt1 FROM @checked_url_id;
+		EXECUTE stmt1 USING @url_table, @this_source, @this_link;
+		DEALLOCATE PREPARE stmt1;
+        IF @temp_id IS NOT NULL THEN
+        	SET @cs = CONCAT('INSERT INTO `?`(`url_id`,`dest_id`) VALUES(?,?)');
+        	PREPARE stmt2 FROM @cs;
+        	EXECUTE stmt2 USING @link_rel_table, @temp_id, @this_dest_id;
+			DEALLOCATE PREPARE stmt2;
         ELSE 
-        	INSERT INTO url_table (`url`,`domain`,`link`,`source`) 
-        	VALUES(this_url , this_domain , this_link , this_source);
-        	SET inserted_url_id = SELECT LAST_INSERT_ID();
-        	INSERT INTO link_rel_table(`url_id`,`dest_id`)
-        	VALUES(last_id_inserted , this_dest_id );
+        	SET @cs = CONCAT('INSERT INTO `?` (`url`,`domain`,`link`,`source`) VALUES(?,?,?,?); SET @temp_id = LAST_INSERT_ID();');
+        	PREPARE stmt3 FROM @cs;					  
+        	EXECUTE stmt3 USING @url_table, @this_url, @this_domain, @this_link, @this_source;
+			DEALLOCATE PREPARE stmt3;
+			SET @cs = CONCAT('INSERT INTO `?`(`url_id`,`dest_id`) VALUES(?,?)');
+			PREPARE stmt4 FROM @cs;					  
+        	EXECUTE stmt4 USING @link_rel_table, @temp_id, @this_dest_id ;
+			DEALLOCATE PREPARE stmt4;
+			SET inserted_url_id = @temp_id;
         END IF;
     END$$
 DELIMITER ;
 
-
+DROP PROCEDURE `url_check`;
 DELIMITER $$
-CREATE FUNCTION url_check(IN url_table varchar(1000), IN link_rel_table varchar(1000), 
+CREATE PROCEDURE url_check(IN url_table varchar(1000), IN link_rel_table varchar(1000), 
 						   IN this_source varchar(1000), IN this_link varchar(1000), 
 						   IN this_url varchar(1000), IN this_domain varchar(1000),
-						   IN this_dest_id int(11) ) RETURNS int(11)						    
-    BEGIN
-        DECLARE checked_url_id int;
-		DECLARE temp_id int;
-        SELECT url_id INTO checked_url_id
-        FROM `url_table` 
-        WHERE source = this_source AND link = this_link;
-        IF checked_url_id IS NOT NULL THEN
-        	INSERT INTO `link_rel_table`(`url_id`,`dest_id`)
-        	VALUES(checked_url_id , this_dest_id );
-        	RETURN -1;
+						   IN this_dest_id int(11), INOUT temp_id int(11) )							    
+		BEGIN
+		SET @url_table = url_table;
+		SET @link_rel_table = link_rel_table;
+		SET @this_source = this_source;
+		SET @this_link = this_link;
+		SET @this_url = this_url; 
+		SET @this_domain = this_domain;
+		SET @this_dest_id = this_dest_id;
+		SET @temp_id = temp_id;
+
+    	SET @checked_url_id = CONCAT('SELECT url_id INTO @temp_id FROM `?` WHERE source = ? AND link = ?');
+		PREPARE stmt1 FROM @checked_url_id;
+		EXECUTE stmt1 USING @url_table, @this_source, @this_link;
+		DEALLOCATE PREPARE stmt1;
+        IF @temp_id IS NOT NULL THEN
+        	SET @cs2 = CONCAT('INSERT INTO `?`(`url_id`,`dest_id`) VALUES(?,?)');
+        	PREPARE stmt2 FROM @cs2;
+        	EXECUTE stmt2 USING @link_rel_table, @temp_id, @this_dest_id;
+			DEALLOCATE PREPARE stmt2;
+			SET @temp_id = 0;
         ELSE 
-        	INSERT INTO `url_table` (`url`,`domain`,`link`,`source`) 
-        	VALUES(this_url , this_domain , this_link , this_source);
-        	SET temp_id = LAST_INSERT_ID();
-        	INSERT INTO `link_rel_table`(`url_id`,`dest_id`)
-        	VALUES(temp_id , this_dest_id );
-			RETURN temp_id;
+        	SET @cs3 = CONCAT('INSERT INTO `?` (`url`,`domain`,`link`,`source`) VALUES(?,?,?,?)');
+        	PREPARE stmt3 FROM @cs3;					  
+        	EXECUTE stmt3 USING @url_table, @this_url, @this_domain, @this_link, @this_source;
+			DEALLOCATE PREPARE stmt3;
+			SET @temp_id = LAST_INSERT_ID;
+			SET @cs4 = CONCAT('INSERT INTO `?`(`url_id`,`dest_id`) VALUES(?,?)');
+			PREPARE stmt4 FROM @cs4;					  
+        	EXECUTE stmt4 USING @link_rel_table, @temp_id, @this_dest_id;
+			DEALLOCATE PREPARE stmt4;
         END IF;
     END$$
 DELIMITER ;
