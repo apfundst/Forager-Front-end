98069cc4f1f7b94f1616aa9bd1ca234a49e8e9c8
Andrew Pfundstein <drew@myskiprofile.com>
2014-04-25 00:47:00 -0400

    GANDDDDOOOLLLLFFFFF!!!!!!!!

--- js/session.js
+++ js/session.js
@@ -527,6 +527,8 @@ var report = {
                     
                         console.log("success");
                         console.log(data);
+                        $("#start_scan").show();
+                        $("#stop_scan").hide();
                         return data;
                         
 
