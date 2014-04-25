e588df9b1b184b114833b665d893e6186c122a79
jcathcart1 <jcathcart1@gmail.com>
2014-04-25 00:48:56 -0400

    I want to buy it all up.....

---
+++ includes/get_pages_with_errors.php
@@ -0,0 +1,31 @@
+<?php
+	$scan_id1 = $_POST["rp1"];
+	$domain1  = $_POST["domain"];
+
+	$con = mysqli_connect("localhost","root","forageme","db_forager");
+	if (mysqli_connect_errno()){
+	  echo "Failed to connect to server" . mysqli_connect_error();
+	  exit;
+	}
+
+	$table1 = "url" . $scan_id1;
+
+	$sql = "
+		SELECT DISTINCT domain FROM `$table1` WHERE domain LIKE '%spsu%' AND state = 0 
+	";
+		
+	$arr = array();
+	$result = mysqli_query($con,$sql);	
+	if(!is_null($result))
+	{
+		while($temp = mysqli_fetch_assoc($result))
+		{
+			$arr[] = $temp;
+		}
+		echo json_encode((array)$arr);
+	}
+	else
+	{
+		echo "NO DIFFERENCES";
+	}	
+?>
--- includes/scan_stop.php
+++ includes/scan_stop.php
@@ -4,10 +4,10 @@ if (mysqli_connect_errno()){
   echo "Failed to connect to server.....can you be cool just once! Just once be cool!: " . mysqli_connect_error();
   exit;
 }
-
 $scan_stop_sql ="
  		UPDATE `scan`
  		SET is_started = 0; 
 ";
 $result = mysqli_query($con,$scan_stop_sql);
+echo json_encode("Scan was stopped!");
 ?>
